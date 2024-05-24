bd36167e5cb0cdab799542b1350d884e9d2e6d04
Admin <admin@Admins-MacBook-Pro-3.local>
2024-05-24 10:27:07 +0530

    main.js file generate when ts file compiled to js

---
+++ main.js
@@ -0,0 +1,12 @@
+var h = document.getElementById("head");
+var but = document.getElementById("but");
+but.onclick = function () {
+    h.innerHTML = "soumya";
+};
+ 
+const obj = {
+    name:"soumya",
+    id:2
+}
+obj.name = "sakshi"
+console.log(obj);
