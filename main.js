c86e20529085583b4c0e72a6d7761e04f76bbb27
Admin <admin@Admins-MacBook-Pro-3.local>
2024-05-24 16:16:51 +0530

    update the main js file

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function Delete(todoId) {
 function addItem(val) {
     var todoId = val.id;
     var ul = document.getElementById("unorder");
-    ul.classList.add("un")
+    ul.classList.add("un");
     //creating list container and added to unorder_list
     var list_container = document.createElement("div");
     list_container.classList.add("cont");
@@ -45,7 +45,7 @@ function addItem(val) {
     list_container.appendChild(list_item);
     // creating delete icon and added to list container
     var delete_icon = document.createElement("i");
-    delete_icon.classList.add("bi", "bi-trash3", "del");
+    delete_icon.classList.add("bi", "bi-x", "del");
     list_container.appendChild(delete_icon);
     //giving onclick event to delete icon
     delete_icon.onclick = function () {
@@ -55,7 +55,6 @@ function addItem(val) {
 //adding addeventlistener to the add button
 input.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        // e.preventDefault();
         var value = input.value;
         if (value === "") {
             alert("enter input");
@@ -73,25 +72,6 @@ input.addEventListener("keydown", function (e) {
         }
     }
 });
-// Add_button.addEventListener("click", function () {
-//   let value = input.value;
-//   if (value === "") {
-//     alert("enter input");
-//   } else {
-//     length1 = length1 + 1;
-//     let b: {
-//       name: string;
-//       id: string;
-//     } = {
-//       name: value,
-//       id: length1,
-//     };
-//     array.push(b);
-//     input.value = "";
-//     addItem(b);
-//     save();
-//   }
-// });
 //creating one function to store array  into the localstorage.
 function save() {
     localStorage.setItem("store", JSON.stringify(array));
