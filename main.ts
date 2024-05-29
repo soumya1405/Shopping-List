79aef1ca2c399a1053a80b9192a6f3e34cd23e73
katta soumya <kattasoumya2002@gmail.com>
2024-05-29 11:53:33 +0530

    update main file by giving proper namings to the variables

--- main.ts
+++ main.ts
@@ -1,32 +1,51 @@
-let input = document.getElementById("in") as HTMLInputElement;
-let Add_button = document.getElementById("add") as HTMLButtonElement;
+let inputElement = document.getElementById("input") as HTMLInputElement;
 let unordered_list = document.createElement("ul") as HTMLUListElement;
-let check = document.createElement("input") as HTMLInputElement;
-check.type = "checkbox";
-check.id = "checkId";
-document.body.appendChild(check);
-let list1:any = [];
-unordered_list.id = "unorder";
+unordered_list.id = "unorderList";
 document.body.appendChild(unordered_list);
-const array = JSON.parse(localStorage.getItem("store")!) || JSON.parse(localStorage.getItem("list")!) || [
-  { name: "Cook Biryani", id: 1 },
-  { name: "Go to Gym", id: 2 },
-  { name: "Study Javascript", id: 3 },
-  { name: "Spend time with plants", id: 4 },
-  { name: "Do Meditation", id: 5 },
-  { name: "Learn Typing", id: 6 },
-  { name: "Do walking", id: 7 },
-  { name: "Watch Movie", id: 8 },
-  { name: "Read Novel", id: 9 },
-  { name: "Talk to friends", id: 10 },
-  { name: "Play ludo", id: 11 },
-];
-//const array = JSON.parse(localStorage.getItem("store"));
-let length1: string = array.length;
+
+let selectedItemsList: string[] = [];
+
+const array = JSON.parse(localStorage.getItem("store")!) ||
+  JSON.parse(localStorage.getItem("list")!) || [
+    { name: "Cook Biryani", id: 1 },
+    { name: "Go to Gym", id: 2 },
+    { name: "Study Javascript", id: 3 },
+    { name: "Spend time with plants", id: 4 },
+    { name: "Do Meditation", id: 5 },
+    { name: "Learn Typing", id: 6 },
+    { name: "Do walking", id: 7 },
+    { name: "Watch Movie", id: 8 },
+    { name: "Read Novel", id: 9 },
+    { name: "Talk to friends", id: 10 },
+    { name: "Play ludo", id: 11 },
+  ];
+
+let div = document.createElement("div") as HTMLDivElement;
+div.classList.add("itemCounts-container");
+document.body.appendChild(div);
+
+let selectedItemsCount = document.createElement("span") as HTMLSpanElement;
+selectedItemsCount.textContent = `Selected Items count : ${selectedItemsList.length}`;
+selectedItemsCount.id = "span1";
+div.appendChild(selectedItemsCount);
+
+let unSelectedItemsCount = document.createElement("span") as HTMLSpanElement;
+unSelectedItemsCount.textContent = `UnSelected Items count : ${
+  array.length - selectedItemsList.length
+}`;
+unSelectedItemsCount.id = "span2";
+div.appendChild(unSelectedItemsCount);
+
+let totalItemsCount = document.createElement("span") as HTMLSpanElement;
+totalItemsCount.textContent = `Total Items Count : ${array.length}`;
+totalItemsCount.id = "span3";
+div.appendChild(totalItemsCount);
+
+let arrayLength: string = array.length;
 for (let val of array) {
   addItem(val);
 }
-
+//delete function to delete items from the unordered list.
 function Delete(todoId: string) {
   let del = document.getElementById(todoId) as HTMLElement;
   unordered_list.removeChild(del);
@@ -35,53 +54,51 @@ function Delete(todoId: string) {
   );
   array.splice(index, 1);
   save();
-  // span3.textContent = `Total Items Count : ${array.length} `;
 }
 function addItem(val: { name: string; id: string }) {
-  let todoId = `todo+${val.id}`;
-  let checkId = val.id;
-  let ul = document.getElementById("unorder") as HTMLElement;
-  ul.classList.add("un");
+  let todoId = `todo+ ${val.id}`;
+  let ul = document.getElementById("unorderList") as HTMLElement;
+  ul.classList.add("unorder-list");
   let list_container = document.createElement("div");
-  list_container.classList.add("cont");
+  list_container.classList.add("list-container");
+  //write a function to show green color when I select the particular item and item will be striked and also written conditions for total count of items
   list_container.addEventListener("click", function (e: MouseEvent) {
-    var ele = e.target as HTMLElement;
-    if (ele.tagName === "LI") {
+    var element = e.target as HTMLElement;
+    if (element.tagName === "LI") {
       list_container.classList.toggle("done");
       if (list_container.classList.contains("done")) {
-        list1.push(ele.tagName);
-        span1.innerHTML = `Selected Items count : ${list1.length}`;
-        var unS = array.length - list1.length;
-        span2.innerHTML = `UnSelected Items count :${String(unS)}`;
+        selectedItemsList.push(element.tagName);
+        selectedItemsCount.innerHTML = `Selected Items count : ${selectedItemsList.length}`;
+        var unSelected = array.length - selectedItemsList.length;
+        unSelectedItemsCount.innerHTML = `UnSelected Items count :${unSelected}`;
       } else {
-        list1.pop(ele.tagName);
-        span1.innerHTML = `Selected Items count : ${list1.length}`;
-        var unS = array.length - list1.length;
-        span2.innerHTML = `UnSelected Items count :${String(unS)}`;
+        selectedItemsList.pop();
+        selectedItemsCount.innerHTML = `Selected Items count : ${selectedItemsList.length}`;
+        var unSelected = array.length - selectedItemsList.length;
+        unSelectedItemsCount.innerHTML = `UnSelected Items count :${unSelected}`;
       }
+      totalItemsCount.textContent = `Total Items Count : ${array.length} `;
     }
-    span3.textContent = `Total Items Count : ${array.length} `;
   });
   list_container.id = todoId;
   ul.appendChild(list_container);
   // creating list item and added to list container
   let list_item = document.createElement("li");
-  list_item.id = "item";
+  list_item.id = todoId;
   list_item.textContent = val.name;
   list_container.appendChild(list_item);
-  // creating delete icon and added to list containerx
+  // creating delete icon and added to list container
   let delete_icon = document.createElement("i");
-  delete_icon.classList.add("bi", "bi-x", "del");
+  delete_icon.classList.add("bi", "bi-x", "delete-icon");
   list_container.appendChild(delete_icon);
   //giving onclick event to delete icon
   delete_icon.onclick = function () {
     Delete(todoId);
   };
+  // write a consition for hiding the selected items, if i click the checkbox then items will be hidden, if I unclick it then it will unhide the all selected items.
   let checkbox = <HTMLInputElement>document.getElementById("checkId");
   checkbox.addEventListener("change", (e) => {
-    console.log(checkbox);
     var isChecked = checkbox.checked;
-    var C = e.target as HTMLElement;
     if (isChecked && list_container.classList.contains("done")) {
       list_container.style.display = "none";
     } else {
@@ -89,45 +106,25 @@ function addItem(val: { name: string; id: string }) {
     }
   });
 }
-
-let div = document.createElement("div") as HTMLDivElement;
-div.classList.add("container");
-document.body.appendChild(div);
-
-let span1 = document.createElement("span") as HTMLSpanElement;
-span1.textContent = `Selected Items count : ${list1.length}`;
-span1.id = "span1";
-div.appendChild(span1);
-
-let span2 = document.createElement("span") as HTMLSpanElement;
-span2.textContent = `UnSelected Items count : ${array.length - list1.length} `;
-span2.id = "span2";
-div.appendChild(span2);
-
-let span3 = document.createElement("span") as HTMLSpanElement;
-span3.textContent = `Total Items Count : ${array.length}`;
-span3.id = "span3";
-div.appendChild(span3);
-
 //adding addeventlistener to the add button
-input.addEventListener("keydown", function (e) {
+inputElement.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
-    let value = input.value;
+    let value = inputElement.value;
     if (value === "") {
       alert("enter input");
     } else {
-      length1 = length1 + 1;
-      let b: {
+      arrayLength = arrayLength + 1;
+      let newItem: {
         name: string;
         id: string;
       } = {
         name: value,
-        id: length1,
+        id: arrayLength,
       };
-      array.push(b);
-      span3.textContent = `Total Items Count : ${array.length} `;
-      input.value = "";
-      addItem(b);
+      array.push(newItem);
+      totalItemsCount.textContent = `Total Items Count : ${array.length} `;
+      inputElement.value = "";
+      addItem(newItem);
       save();
     }
   }
