74130f51a0a627ce9e5e4eb8e61c11716503bca1
katta soumya <kattasoumya2002@gmail.com>
2024-05-29 11:53:47 +0530

    update main file by giving proper namings to the variables

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-var input = document.getElementById("in");
-var Add_button = document.getElementById("add");
+var inputElement = document.getElementById("input");
 var unordered_list = document.createElement("ul");
-var check = document.createElement("input");
-check.type = "checkbox";
-check.id = "checkId";
-document.body.appendChild(check);
-var list1 = [];
-unordered_list.id = "unorder";
+unordered_list.id = "unorderList";
 document.body.appendChild(unordered_list);
-var array = JSON.parse(localStorage.getItem("store")) || JSON.parse(localStorage.getItem("list")) || [
+var selectedItemsList = [];
+var array = JSON.parse(localStorage.getItem("store")) ||
+    JSON.parse(localStorage.getItem("list")) || [
     { name: "Cook Biryani", id: 1 },
     { name: "Go to Gym", id: 2 },
     { name: "Study Javascript", id: 3 },
@@ -21,71 +17,79 @@ var array = JSON.parse(localStorage.getItem("store")) || JSON.parse(localStorage
     { name: "Talk to friends", id: 10 },
     { name: "Play ludo", id: 11 },
 ];
-//const array = JSON.parse(localStorage.getItem("store"));
-var length1 = array.length;
+var div = document.createElement("div");
+div.classList.add("itemCounts-container");
+document.body.appendChild(div);
+var selectedItemsCount = document.createElement("span");
+selectedItemsCount.textContent = "Selected Items count : ".concat(selectedItemsList.length);
+selectedItemsCount.id = "span1";
+div.appendChild(selectedItemsCount);
+var unSelectedItemsCount = document.createElement("span");
+unSelectedItemsCount.textContent = "UnSelected Items count : ".concat(array.length - selectedItemsList.length);
+unSelectedItemsCount.id = "span2";
+div.appendChild(unSelectedItemsCount);
+var totalItemsCount = document.createElement("span");
+totalItemsCount.textContent = "Total Items Count : ".concat(array.length);
+totalItemsCount.id = "span3";
+div.appendChild(totalItemsCount);
+var arrayLength = array.length;
 for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
     var val = array_1[_i];
     addItem(val);
 }
+//delete function to delete items from the unordered list.
 function Delete(todoId) {
     var del = document.getElementById(todoId);
     unordered_list.removeChild(del);
     var index = array.findIndex(function (value) { return value.id === todoId; });
     array.splice(index, 1);
     save();
-    // span3.textContent = `Total Items Count : ${array.length} `;
 }
 function addItem(val) {
-    var todoId = "todo+".concat(val.id);
-    var checkId = val.id;
-    var ul = document.getElementById("unorder");
-    ul.classList.add("un");
-    // let check = document.createElement("input") as HTMLInputElement;
-    // check.type = "checkbox";
-    // check.id = checkId;
-    // ul.appendChild(check);
-    //creating list container and added to unorder_list
+    var todoId = "todo+ ".concat(val.id);
+    var ul = document.getElementById("unorderList");
+    ul.classList.add("unorder-list");
     var list_container = document.createElement("div");
-    list_container.classList.add("cont");
+    list_container.classList.add("list-container");
+    //write a function to show green color when I select the particular item and item will be striked and also written conditions for total count of items
     list_container.addEventListener("click", function (e) {
-        var ele = e.target;
-        if (ele.tagName === "LI") {
+        var element = e.target;
+        if (element.tagName === "LI") {
             list_container.classList.toggle("done");
             if (list_container.classList.contains("done")) {
-                list1.push(ele.tagName);
-                span1.innerHTML = "Selected Items count : ".concat(list1.length);
-                var unS = array.length - list1.length;
-                span2.innerHTML = "UnSelected Items count :".concat(String(unS));
+                selectedItemsList.push(element.tagName);
+                selectedItemsCount.innerHTML = "Selected Items count : ".concat(selectedItemsList.length);
+                var unSelected = array.length - selectedItemsList.length;
+                unSelectedItemsCount.innerHTML = "UnSelected Items count :".concat(unSelected);
             }
             else {
-                list1.pop(ele.tagName);
-                span1.innerHTML = "Selected Items count : ".concat(list1.length);
-                var unS = array.length - list1.length;
-                span2.innerHTML = "UnSelected Items count :".concat(String(unS));
+                selectedItemsList.pop();
+                selectedItemsCount.innerHTML = "Selected Items count : ".concat(selectedItemsList.length);
+                var unSelected = array.length - selectedItemsList.length;
+                unSelectedItemsCount.innerHTML = "UnSelected Items count :".concat(unSelected);
             }
+            totalItemsCount.textContent = "Total Items Count : ".concat(array.length, " ");
         }
-        span3.textContent = "Total Items Count : ".concat(array.length, " ");
     });
     list_container.id = todoId;
     ul.appendChild(list_container);
     // creating list item and added to list container
     var list_item = document.createElement("li");
-    list_item.id = "item";
+    list_item.id = todoId;
     list_item.textContent = val.name;
     list_container.appendChild(list_item);
-    // creating delete icon and added to list containerx
+    // creating delete icon and added to list container
     var delete_icon = document.createElement("i");
-    delete_icon.classList.add("bi", "bi-x", "del");
+    delete_icon.classList.add("bi", "bi-x", "delete-icon");
     list_container.appendChild(delete_icon);
     //giving onclick event to delete icon
     delete_icon.onclick = function () {
         Delete(todoId);
     };
+    // write a consition for hiding the selected items, if i click the checkbox then items will be hidden, if I unclick it then it will unhide the all selected items.
     var checkbox = document.getElementById("checkId");
     checkbox.addEventListener("change", function (e) {
-        console.log(checkbox);
         var isChecked = checkbox.checked;
-        var C = e.target;
         if (isChecked && list_container.classList.contains("done")) {
             list_container.style.display = "none";
         }
@@ -94,38 +98,23 @@ function addItem(val) {
         }
     });
 }
-var div = document.createElement("div");
-div.classList.add("container");
-document.body.appendChild(div);
-var span1 = document.createElement("span");
-span1.textContent = "Selected Items count : ".concat(list1.length);
-span1.id = "span1";
-div.appendChild(span1);
-var span2 = document.createElement("span");
-span2.textContent = "UnSelected Items count : ".concat(array.length - list1.length, " ");
-span2.id = "span2";
-div.appendChild(span2);
-var span3 = document.createElement("span");
-span3.textContent = "Total Items Count : ".concat(array.length);
-span3.id = "span3";
-div.appendChild(span3);
 //adding addeventlistener to the add button
-input.addEventListener("keydown", function (e) {
+inputElement.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        var value = input.value;
+        var value = inputElement.value;
         if (value === "") {
             alert("enter input");
         }
         else {
-            length1 = length1 + 1;
-            var b = {
+            arrayLength = arrayLength + 1;
+            var newItem = {
                 name: value,
-                id: length1,
+                id: arrayLength,
             };
-            array.push(b);
-            span3.textContent = "Total Items Count : ".concat(array.length, " ");
-            input.value = "";
-            addItem(b);
+            array.push(newItem);
+            totalItemsCount.textContent = "Total Items Count : ".concat(array.length, " ");
+            inputElement.value = "";
+            addItem(newItem);
             save();
         }
     }
@@ -133,5 +122,4 @@ input.addEventListener("keydown", function (e) {
 //creating one function to store array  into the localstorage.
 function save() {
     localStorage.setItem("store", JSON.stringify(array));
-    // localStorage.setItem("list", JSON.stringify(list1));
 }
